cb4c7531baebe697ee7227fa684d799cdd250865
Rudy Bermúdez <hello@rudybermudez.io>
2019-09-10 12:57:55 -0700

    Initial Commit

---
+++ WeatherStatus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WeatherStatus
+//
+//  Created by Rudy Bermudez on 9/10/19.
+//  Copyright © 2019 Rudy Bermudez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WeatherStatus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeatherStatus
+//
+//  Created by Rudy Bermudez on 9/10/19.
+//  Copyright © 2019 Rudy Bermudez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
